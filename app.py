830af76c73a894f7329513a9788118d854819603
vellorejana <77191238+vellorejana@users.noreply.github.com>
2021-12-14 19:08:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,4 @@ def predict():
 
 if __name__ == '__main__':
     print('***App Started***')
-    app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-    });
+    app.run(debug=True)
